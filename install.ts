1ef5743df3878a0d43d07f7a72ee8ff81ad12ba4
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-08-20 07:03:23 +0300

    Update install.ts

--- install.ts
+++ install.ts
@@ -2,6 +2,8 @@ import { $ } from "bun";
 import os from "os";
 import process from "process";
 
+const make = (os.platform() != "win32") ? "make" : "cmake"
+
 // Installing sanjuuni
 if (os.platform() != "win32") {
     await $`sudo apt install ffmpeg`
@@ -10,11 +12,18 @@ if (os.platform() != "win32") {
 } else {
     await $`winget install ffmpeg`
 }
-await $`git clone https://github.com/MCJack123/sanjuuni --recurse-submodules`
+await $`git clone https://github.com/MCJack123/sanjuuni --recurse-submodules`.nothrow()
 process.chdir("sanjuuni")
-await $`./configure`
-await $`make`
+
+if (os.platform() != "win32") {
+    await $`./configure`
+}
+await $`${make}`
 
 // Installing YouCube
 process.chdir("../")
-await $`make install-requirements`
+if (os.platform() != "win32") {
+    await $`${make} install-requirements`
+} else {
+    await $`${make} --build install-requirements`
+}
