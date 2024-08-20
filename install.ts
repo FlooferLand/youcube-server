2e6f1ec076cd6c265422ebf8e53f0623fd228e64
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-08-20 07:32:45 +0300

    Update install.ts

--- install.ts
+++ install.ts
@@ -6,11 +6,11 @@ const make = (os.platform() != "win32") ? "make" : "cmake"
 
 // Installing sanjuuni
 if (os.platform() != "win32") {
-    await $`sudo apt install ffmpeg`
-    await $`sudo apt install libavcodec53`
-    await $`sudo apt install libavformat53`
+    await $`sudo apt install ffmpeg`.nothrow()
+    await $`sudo apt install libavcodec53`.nothrow()
+    await $`sudo apt install libavformat53`.nothrow()
 } else {
-    await $`winget install ffmpeg`
+    await $`winget install ffmpeg`.nothrow()
 }
 await $`git clone https://github.com/MCJack123/sanjuuni --recurse-submodules`.nothrow()
 process.chdir("sanjuuni")
